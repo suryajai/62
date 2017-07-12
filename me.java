481f768f20d3f8644178da7c80271bdf2b48ad97
suryajai <arasidpharm@gmail.com>
2017-07-12 09:54:52 +0530

    Add files via upload

---
+++ me.java
@@ -0,0 +1,17 @@
+
+public class me {
+	public static void main(String[] args) {
+
+        double n1 = 4, n2 = 3, n3 = 2.2;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
+
